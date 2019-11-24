c753c2cbcd84408f82dc5688f66ab20e7d2c9d33
mokdeveloper <mok.developer@gmail.com>
2019-11-24 22:48:31 +0500

    fix: file size and type fix

--- src/validations/schemas/file.ts
+++ src/validations/schemas/file.ts
@@ -8,12 +8,27 @@ export const uploadUserProfileImage: Joi.SchemaMap = {
         .required()
         .less(FILE_MAX_SIZE)
         .greater(FILE_MIN_SIZE)
-        .error(new Error('File Size must be less than 2mb')),
+        .error(new Error('File Size must be less than 5mb')),
       path: Joi.string().required(),
       name: Joi.string().required(),
       type: Joi.string()
         .required()
         .valid(...SUPPORTED_PICTURE_TYPES),
+    })
+    .required(),
+};
+
+export const uploadExecutionFile: Joi.SchemaMap = {
+  file: Joi.object()
+    .keys({
+      size: Joi.number()
+        .required()
+        .less(FILE_MAX_SIZE)
+        .greater(FILE_MIN_SIZE)
+        .error(new Error('File Size must be less than 5mb')),
+      path: Joi.string().required(),
+      name: Joi.string().required(),
+      type: Joi.string(),
     })
     .required(),
 };
